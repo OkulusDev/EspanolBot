da447d251bd993549e8810e2619c0de54c3d8fca
Okulus Dev <132923572+OkulusDev@users.noreply.github.com>
2023-05-08 22:26:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 Создатель: Okulus Dev (C) 2023 ALL RIGHTS REVERSED | ВСЕ ПРАВА СОХРАНЕНЫ
 Лицензия: GNU GPL v3"""
 
-TOKEN = '6073401934:AAGKoEqhTsC1VnLe3aP0V963sEMiR0ZTmqw'
-BOT_NAME = 'https://t.me/espanol_russian_learning_bot'
+TOKEN = 'enter bot token here'
+BOT_NAME = 'enter bot name here'
 
 ADMINS_IDS = [5489665107,]
