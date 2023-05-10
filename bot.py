e81da81a6722f89830ebc490577f8cce98aa8172
Okulus Dev <132923572+OkulusDev@users.noreply.github.com>
2023-05-10 23:46:22 +0700

    Update with report system
    В этом обновлении добавлена система репортов

--- bot.py
+++ bot.py
@@ -2,22 +2,28 @@
 # -*- coding:utf-8 -*-
 """Телеграм-бот для изучения испанского языка. Данный бот позволит вам 
 Создатель: Okulus Dev (C) 2023 ALL RIGHTS REVERSED | ВСЕ ПРАВА СОХРАНЕНЫ
-Лицензия: GNU GPL v3"""
+Лицензия: GNU GPL v3
+Версия: 1.1.2 BETA"""
+
+# Импорт стандартных библиотек
 import sys													# Импорт системной библиотеки
 import os													# Импорт OS
 import logging                                              # Импорт библиотеки логирования
 import re 													# Импорт библиотеки для регулярных выражений
-from random import choice
+from random import choice									# Импорт библиотеки рандома
+from datetime import datetime								# Импорт библиотеки даты и времени
 
-import telebot												# Импорт pytelegrambotapi
+# Импорт сторонних библиотек
+import telebot												# Импорт pytelegrambotapi (telebot)
 from telebot import types									# Импорт типов pytelegrambotapi
 from youtube_transcript_api import YouTubeTranscriptApi     # Импорт Youtube Transcript Api
-from deep_translator import GoogleTranslator                # Импорт переводчика
+from deep_translator import GoogleTranslator                # Импорт переводчика Google
 
-import config as cfg 										# Импорт конфигурации
+# Импорт существующих файлов
+import config as cfg 										# Импорт конфигурации бота
 import datalog                                              # Импорт библиотеки для сбора данных
 import dbhelper												# Импорт библиотеки для взаимодействия с базой данных
-import lessons                                              # Импорт уроков
+import lessons                                              # Импорт уроков и тестов
 
 # Создание бота и базы данных
 bot = telebot.TeleBot(cfg.TOKEN)
@@ -25,14 +31,17 @@ db_helper = dbhelper.DBHelper()
 
 # Настройка логгера
 logger = telebot.logger
-logger.setLevel(level=logging.INFO)
-filehandler = logging.FileHandler('bot.log', mode='a')
-formatter_handler = logging.Formatter('%(name)s %(asctime)s [%(levelname)s]: %(message)s')
-filehandler.setFormatter(formatter_handler)
-logger.addHandler(filehandler)
+logger.setLevel(level=logging.INFO)						# Установка уровня INFO в логгере
+filehandler = logging.FileHandler('bot.log', mode='a')      # Создание лог-файла 
+formatter_handler = logging.Formatter(						# Создание формата строк для логов
+					'%(name)s %(asctime)s [%(levelname)s]: %(message)s')
+filehandler.setFormatter(formatter_handler) 				# Установка формата
+logger.addHandler(filehandler)								# Установка обработчика
 
+# Для ошибок
 START_OVER, ADDING, END = range(1, -2, -1)
-# Регулярное выражение для ссылки на видео ютуба
+
+# Регулярное выражение для ссылки на youtube-видео
 re_youtube = re.compile('^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+')
 re_text = re.compile('^[a-z]{3,20}$')
 
@@ -60,34 +69,43 @@ def clear_text(text: str) -> str:
 
 
 def add_words(message):
-	"""Транскрипция на испанский ютуб-видео."""
+	"""Транскрипция на испанский youtube-видео. Данная функция использует Youtube Transcript API
+	для преобразования речи в текст на испанском языке"""
 	bot.send_message(message.chat.id, 'Загрузка... Подождите...')
 
 	if is_youtube_link(message.text):
+		# Если ссылка проходит валидацию
 		try:
+			# Попытка транскрипции
 			logger.info(f'Попытка транскрипции youtube-видео {message.text}')
 			transcript_list = YouTubeTranscriptApi.list_transcripts(message.text.split('/')[-1])
 			t = transcript_list.find_transcript(['es'])
 			text = clear_text('.'.join([i['text'] for i in t.fetch()]))
 
+			# Сохранение в файл
 			with open('trans.txt', 'w') as file:
 				logger.info(f'Сохранение транскрипции {message.text} в файл')
 				file.write(text)
 
+			# Отправка файла пользователя
 			with open("trans.txt", 'rb') as file:
-				logger.info('Отправка файла с транскрипцией пользователю')
+				logger.info(f'Отправка файла с транскрипцией {message.text} пользователю')
 				bot.send_document(message.chat.id, file)
 
+			# Удаление файла
 			logger.info(f'Удаление файла с транскрипцией {message.text}')
 			os.system('rm trans.txt')
 
+			# Повышение уровня
 			db_helper.update_level(message.from_user.id, int(db_helper.get_user_level(message.from_user.id)[0][0]) + 1)
 			bot.send_message(message.chat.id, 'Ваш уровень владения языком повышен!')
 		except Exception as ex:
+			# При исключении, выводим информацию о ошибке
 			print(ex)
 			logger.error(f'Ошибка обработки видео {message.text}: {ex}')
 			bot.send_message(message.chat.id, 'Некорректное или без субтитров видео. Попробуйте еще раз!')
-	else:''
+	else:
+		# Если ссылка не прошла валидацию, уведомим об этом пользователя
 		bot.send_message(message.chat.id, 'Это не youtube-видео!')
 
                                                                                                
@@ -95,20 +113,27 @@ def translate_esp2rus(message):
 	"""Перевод с испанского на русский"""
 	translator = GoogleTranslator(source='es', target='ru')
 	result = translator.translate(message.text)
+
 	bot.send_message(message.chat.id, f'Оригинал: {message.text}\nПеревод: {result}')
+	
 	db_helper.update_level(message.from_user.id, int(db_helper.get_user_level(message.from_user.id)[0][0]) + 1)
 	bot.send_message(message.chat.id, 'Ваш уровень владения языком повышен!')
 
 
 def test_translate(message, word):
+	"""Тест на знание слова. Данная функция требует от пользователя перевод слова на испанский
+	Аргумент:
+	 + word - исходное слово"""
 	translator = GoogleTranslator(source='ru', target='es')
 	result = translator.translate(word)
 
 	if message.text.lower().strip() == result.lower().strip():
+		# Если перевод соответствует, то уведомляем пользователя об этом и повышаем его уровень
 		bot.send_message(message.chat.id, 'Абсолютно правильно!')
 		db_helper.update_level(message.from_user.id, int(db_helper.get_user_level(message.from_user.id)[0][0]) + 1)
 		bot.send_message(message.chat.id, 'Ваш уровень владения языком повышен!')
 	else:
+		# Если перевод неверен, то уведомим пользователя об этом и понижаем его уровень
 		bot.send_message(message.chat.id, f'Неверно! Правильный перевод - {result}')
 		db_helper.update_level(message.from_user.id, int(db_helper.get_user_level(message.from_user.id)[0][0]) - 1)
 		bot.send_message(message.chat.id, 'Ваш уровень владения языком понижен!')
@@ -126,32 +151,52 @@ def translate_rus2esp(message):
 def bot_start_alert():
 	# Предупреждение администраторов о том, что бот был запущен
 	for admin_id in cfg.ADMINS_IDS:
+		# Проходимся в цикле по списку ID администраторов
 		bot.send_message(admin_id, "Внимание! Бот @espanol_russian_learning_bot запущен!")
 
 
 def delete_user_from_db(message):
+	# Удаляем пользователя из базы данных
 	db_helper.delete_user(message.text)
 	bot.send_message(message.chat.id, f'Пользователь {message.text} удален!')
 
 
 def add_user_to_db(message):
+	# Добавляем пользователя в базу данных
 	db_helper.add_user(message.text)
 	bot.send_message(message.chat.id, f'Пользователь {message.text} добавлен!')
 
 
 def set_level_to_user_first(message):
+	# Установка уровня для пользователя, шаг первый.
 	mesg = bot.send_message(message.chat.id, f'Введите новое значение уровня пользователя {message.text}')
 	bot.register_next_step_handler(mesg, set_level_to_user_end, message.text)
 
 
 def set_level_to_user_end(message, user_id):
+	# Установка уровня для пользователя, шаг второй (последний)
 	db_helper.update_level(user_id, message.text)
 	bot.send_message(message.chat.id, f'Уровень пользователя {user_id} теперь {message.text}!')
+
+
+def send_report(message):
+	# Функция отправки репорта разработчикам
+	user_id = message.from_user.id
+	time = datetime.now()
+	for admin in cfg.ADMINS_IDS:
+		bot.send_message(admin, f'''Здравствуйте, администратор {admin}!
+Пользователь под ID {user_id} прислал репорт:\n
+{message.text}''')
+		bot.send_message(message.chat.id, f'Репорт сохранен в файл "report_{user_id}_{time}.txt"')
+	with open(f'report_{user_id}_{time}.txt', 'w') as file:
+		file.write(message.text)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
 	# Команда /start
+
+	# Создаем клавиатуру
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	item0 = types.KeyboardButton('Админ-панель')
 	item1 = types.KeyboardButton('Профиль')
@@ -159,12 +204,16 @@ def start_message(message):
 	item3 = types.KeyboardButton('Расшифровка видео')
 	item4 = types.KeyboardButton('Переводчик')
 	item5 = types.KeyboardButton('Уроки')
-	item6 = types.KeyboardButton('Перевод случайного слова')
+	item6 = types.KeyboardButton('Тест на знание испанских слов')
 	item7 = types.KeyboardButton('Материалы для изучения')
+	item8 = types.KeyboardButton('О нас')
+	item9 = types.KeyboardButton('Правила отправки репорта')
+	item10 = types.KeyboardButton('Отправить репорт разработчикам')
 	markup.add(item1, item2)
 	markup.add(item3, item4)
 	markup.add(item5, item6)
-	markup.add(item7)
+	markup.add(item7, item8)
+	markup.add(item9, item10)
 
 	# Регистрация пользователя в базе данных
 	if message.from_user.id not in db_helper.get_users_ids():
@@ -175,6 +224,7 @@ def start_message(message):
 		bot.send_message(message.chat.id, 'Приветствую вас, администратор!')
 		markup.add(item0)
 
+	# Приветственное сообщение
 	bot.send_message(message.chat.id, 'Привет! Я бот для изучения испанского языка!', reply_markup=markup)
 
 
@@ -217,6 +267,34 @@ def text_messages(message):
 		for i in data:
 			result += str(f'ID: {i[0]}. Уровень: {i[1]}\n')
 		bot.send_message(chatid, result)
+	elif msgtext == 'О нас':
+		bot.send_message(chatid, '''Бот для бесплатного изучения испанского языка.
+Данный бот позволит вам бесплатно изучать испанский язык. Здесь вы можете оттачить свои знания слов, лексики, грамматики.
+Этот бот вам предлагает переводить YouTube-видео на испанский текст, проходить тесты, а также он имеет свою таблицу пользователей!
+Вы можете становиться лучше, повышая свой уровень до высот!
+Создатель: @OkulusDev
+Лицензия: GNU GPL v3
+Исходный код (GitHub): https://github.com/OkulusDev/EspanolBot/
+			''')
+	elif msgtext == 'Правила отправки репорта':
+		bot.send_message(chatid, '''Вы заметили баг в боте? Вы перевели слово правильно, а вам не засчитали?
+Составьте репорт разработчикам! Они обязательно узнают о баге и будут активно его исправлять!
+Или у вас есть пожелания? То можете тоже составить репорт, чтобы разработчики добавили ваши желания в ближайшее время.
+
+Текст составления репорта такой:
+Здравствуйте, разработчики EspanolBot!
+[Расскажите о багах или пожеланиях]''')
+	elif msgtext == 'Отправить репорт разработчикам':
+		bot.send_message(chatid, 'Для того, чтобы ваш репорт точно был прочитан, прочитайте правила отправки репорта:')
+		bot.send_message(chatid, '''Вы заметили баг в боте? Вы перевели слово правильно, а вам не засчитали?
+Составьте репорт разработчикам! Они обязательно узнают о баге и будут активно его исправлять!
+Или у вас есть пожелания? То можете тоже составить репорт, чтобы разработчики добавили ваши желания в ближайшее время.
+
+Текст составления репорта такой:
+Здравствуйте, разработчики EspanolBot!
+[Расскажите о багах или пожеланиях]''')
+		report = bot.send_message(chatid, 'Введите текст репорта:')
+		bot.register_next_step_handler(report, send_report)
 	elif msgtext == 'Админ-панель':
 		markup = types.InlineKeyboardMarkup()
 		markup.add(types.InlineKeyboardButton(text='Удалить пользователя', callback_data='delete_user'))
--- dbhelper.py
+++ dbhelper.py
@@ -16,50 +16,72 @@ class DBHelper:
 		self.conn = sqlite3.connect(dbname, check_same_thread=False)
 		self.cursor = self.conn.cursor()
 
-	def setup(self):
-		# Создание таблицы
+	def setup(self) -> None:
+		"""Создание таблицы. Данная функция создает таблицу в базе данных, если она не создана, 
+		со значениями id - ID пользователя и level - уровнем пользователя. 
+		id является цифровым объектом, а level строковым."""
 		sql = 'CREATE TABLE IF NOT EXISTS users (id integer, level text)'
 		self.conn.execute(sql)
 		self.conn.commit()
 
-	def add_user(self, user_id, level=0):
-		# Добавление пользователя
+	def add_user(self, user_id, level=0) -> None:
+		"""Добавление пользователя: данная функция добавляет пользователя в базу данных
+		с его уникальным идентификатором - ID и уровнем, который по умолчанию равен 0
+		Аргументы:
+		 + user_id - ID пользователя
+		 + level - уровень пользователя (значение по умолчанию = 0)"""
 		sql = 'INSERT INTO users (id, level) VALUES (?, ?)'
 		args = (user_id, level)
 		self.conn.execute(sql, args)
 		self.conn.commit()
 
-	def delete_user(self, user_id):
-		# Удаление пользователя
+	def delete_user(self, user_id) -> None:
+		"""Удаление пользователя по ID в базе данных. Данная функция удаляет пользователя из
+		базы данных, используя его ID.
+		Аргументы:
+		 + user_id - ID пользователя"""
 		sql = 'DELETE FROM users WHERE id = (?)'
 		args = (user_id, )
 		self.conn.execute(sql, args)
 		self.conn.commit()
 
 	def get_user_level(self, user_id):
-		# Получить уровень пользователя по ID
+		"""Получение уровня пользователя. Данная функция получает уровень пользователя по ID
+		Аргументы:
+		 + user_id - ID пользователя
+		Возвращает:
+		 + data - информация о пользователе"""
 		sql = 'SELECT level FROM users WHERE id = ?'
 		args = (user_id, )
 		data = self.conn.execute(sql, args).fetchall()
 		self.conn.commit()
+
 		return data
 
-	def update_level(self, user_id, new_level):
-		# Обновить уровень пользователя по ID
+	def update_level(self, user_id, new_level) -> None:
+		"""Обновление уровня. Данная функция обновляет уровень пользователя по ID.
+		Аргументы:
+		 + user_id - ID пользователя
+		 + new_level - новое значение уровня"""
 		sql = 'UPDATE users SET level = ? WHERE id = ?'
 		args = (new_level, user_id)
 		self.conn.execute(sql, args)
 		self.conn.commit()		
 
 	def get_users_ids(self):
-		# Получение ID пользователей
+		"""Получение всех ID пользователей. Данная функция получает только все ID пользователей
+		в базе"""
 		sql = 'SELECT id FROM users'
 		return [x[0] for x in self.conn.execute(sql)]
 
-	def get_users(self):
-		# Получение данных пользователей
+	def get_users(self) -> list:
+		"""Получение всех пользователей.. Данная функция получает всех пользователей, вместе
+		с их ID и уровнем.
+		Возвращает:
+		 + list (список) - список с информацией о пользователях"""
 		sql = 'SELECT id, level FROM users'
 		result = []
 		for i in self.conn.execute(sql):
 			result.append(i)
+		
 		return result
